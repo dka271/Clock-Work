3b25befee528f2c8ca90c2c00b9c2a079641b464
Daniel Amick <dka271@vt.edu>
2016-01-29 21:32:34 -0500

    Script to move objects ritualistically

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class Mover : MonoBehaviour {
 
+    public float moveTime;
+    public Vector2 moveSpeed;
+
 	// Use this for initialization
 	void Start () {
-	
+        StartCoroutine(timer());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        transform.Translate(moveSpeed * Time.deltaTime);
+    }
+
+    IEnumerator timer()
+    {
+        while (true) { 
+            yield return new WaitForSeconds(moveTime);
+            moveSpeed *= -1;
+    }
+
+    }
 }

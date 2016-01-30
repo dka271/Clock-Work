c55f520050e1fa1a5949ff5438c97f3669bdf7f4
aserv <alexs9@vt.edu>
2016-01-30 13:37:03 -0500

    Player now checks for ground on sides rather than center

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,17 @@ public class Player : MonoBehaviour {
     public float walkingAccel;
     public float jumpSpeed;
     public float glideFallSpeed;
+    /*enum PlayerState {
+        Dead,
+        Idle,
+        Walking,
+        Pushing,
+        Jumping,
+        Gliding
+    }
+    private PlayerState state;*/
     private float raycastDistance;
+    private Vector2 boxSide;
     private bool isGrounded;
     private float horizontalVelocity;
     private new Rigidbody2D rigidbody2D;
@@ -17,14 +27,18 @@ public class Player : MonoBehaviour {
 	void Start () {
         rigidbody2D = this.GetComponent<Rigidbody2D>();
         raycastDistance = 1.5f * this.GetComponent<Collider2D>().bounds.extents.y;
+        boxSide = new Vector2(this.GetComponent<Collider2D>().bounds.extents.x, 0);
+        //state = PlayerState.Idle;
 	}
 	
 	void Update () {
         float horiz = Input.GetAxis("Horizontal");
-        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, raycastDistance, -1 ^ (1 << 9));
+        isGrounded = 
+            Physics2D.Raycast((Vector2)transform.position + boxSide, Vector2.down, raycastDistance, -1 ^ (1 << 9)) || 
+            Physics2D.Raycast((Vector2)transform.position - boxSide, Vector2.down, raycastDistance, -1 ^ (1 << 9));
         if (isGrounded) {
             if (Input.GetButtonDown("Vertical"))
-                rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpSpeed);
+                rigidbody2D.velocity += new Vector2(0, jumpSpeed);
         } else {
             if (Input.GetButton("Vertical"))
                 rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, Mathf.Clamp(rigidbody2D.velocity.y, glideFallSpeed, float.MaxValue));

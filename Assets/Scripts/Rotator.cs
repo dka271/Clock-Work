a7a56d4c32cf95e15dac1ac5fc8d4f7d04d3d264
aserv <alexs9@vt.edu>
2016-01-29 21:06:34 -0500

    Added in utility files

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+    public float angularSpeed;
+
+	// Update is called once per frame
+	void Update () {
+	    transform.Rotate(Vector3.forward, angularSpeed * Time.deltaTime);
+	}
+}

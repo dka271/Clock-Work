e1391aa1470f9a7d3b76eda8b61654e2f134bf00
aserv <alexs9@vt.edu>
2016-01-30 15:35:53 -0500

    Restructured Art Folder, Constructed Player Animator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,45 +8,69 @@ public class Player : MonoBehaviour {
     public float walkingAccel;
     public float jumpSpeed;
     public float glideFallSpeed;
-    /*enum PlayerState {
-        Dead,
-        Idle,
-        Walking,
-        Pushing,
-        Jumping,
-        Gliding
-    }
-    private PlayerState state;*/
     private float raycastDistance;
     private Vector2 boxSide;
     private bool isGrounded;
+    private bool isDead;
     private float horizontalVelocity;
     private new Rigidbody2D rigidbody2D;
+    private Animator anim;
+
+    private static int animIdJump, animIdWalking, animIdGrounded, animIdKilled, animIdGliding;
 
 	// Use this for initialization
 	void Start () {
         rigidbody2D = this.GetComponent<Rigidbody2D>();
         raycastDistance = 1.5f * this.GetComponent<Collider2D>().bounds.extents.y;
         boxSide = new Vector2(this.GetComponent<Collider2D>().bounds.extents.x, 0);
-        //state = PlayerState.Idle;
+        anim = this.GetComponent<Animator>();
+        animIdJump = Animator.StringToHash("Jump");
+        animIdWalking = Animator.StringToHash("Walking");
+        animIdGrounded = Animator.StringToHash("Grounded");
+        animIdKilled = Animator.StringToHash("Killed");
+        animIdGliding = Animator.StringToHash("Gliding");
 	}
 	
+    //Called Every Frame
 	void Update () {
+        if (Input.GetButtonDown("Respawn")) { //Kills player
+            isDead = !isDead;
+            if (isDead) {
+                horizontalVelocity = 0;
+                anim.SetTrigger(animIdKilled);
+            } else {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+        if (isDead) //Don't handle actions if dead
+            return;
         float horiz = Input.GetAxis("Horizontal");
         isGrounded = 
             Physics2D.Raycast((Vector2)transform.position + boxSide, Vector2.down, raycastDistance, -1 ^ (1 << 9)) || 
             Physics2D.Raycast((Vector2)transform.position - boxSide, Vector2.down, raycastDistance, -1 ^ (1 << 9));
+        anim.SetBool(animIdGrounded, isGrounded);
         if (isGrounded) {
-            if (Input.GetButtonDown("Vertical"))
+            anim.SetBool(animIdGliding, false);
+            if (Input.GetButtonDown("Vertical")) {
                 rigidbody2D.velocity += new Vector2(0, jumpSpeed);
+                anim.SetTrigger(animIdJump);
+            }
         } else {
-            if (Input.GetButton("Vertical"))
+            if (Input.GetButton("Vertical")) {
                 rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, Mathf.Clamp(rigidbody2D.velocity.y, glideFallSpeed, float.MaxValue));
+                anim.SetBool(animIdGliding, rigidbody2D.velocity.y < 0);
+            }
+        }
+        if (transform.localScale.x * horiz < 0) { //Invert Direction if Needed
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
         horizontalVelocity = horiz * walkingSpeed;
+        anim.SetBool(animIdWalking, horizontalVelocity != 0 && isGrounded);
 	}
 
+    //Called Every Physics Step
     void FixedUpdate() {
+        //Accelerate/Decelerate toward intended velocity
         rigidbody2D.velocity = new Vector2(
             Mathf.MoveTowards(rigidbody2D.velocity.x, horizontalVelocity, 
                 (rigidbody2D.velocity.x * horizontalVelocity > 0 ? 1 : 5) * walkingAccel * Time.fixedDeltaTime),

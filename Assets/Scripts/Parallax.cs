65aa42f318035d05ee379de4dc0accef617aff30
aserv <alexs9@vt.edu>
2016-01-30 22:20:54 -0500

    Lots. Piston Rework, Menu Framework, Script for Parallax, Script for Piston

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+    public GameObject fg;
+    public float ratio;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(fg.transform.position.x * ratio, fg.transform.position.y * ratio, transform.position.z);
+	}
+}
---
+++ Assets/Scripts/Piston.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piston : Mover {
+	// Update is called once per frame
+	void Update () {
+        transform.parent.transform.Translate(moveSpeed * Time.deltaTime);
+        transform.Translate(-moveSpeed * Time.deltaTime / 2);
+        transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + Time.deltaTime * moveSpeed.y/2, transform.localScale.z);
+	}
+}

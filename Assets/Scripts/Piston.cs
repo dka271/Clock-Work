6ac18e129c1a8b7c55c227d19e1b26b218380f93
rlane98 <rlane98@vt.edu>
2016-01-31 15:58:14 -0500

    Intro_glide, Hallway, and Maze are completed (except timer, background, and goal).  piston was updated for upside-down pistons.

--- Assets/Scripts/Piston.cs
+++ Assets/Scripts/Piston.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 [RequireComponent(typeof(Collider2D))]
 public class Piston : MonoBehaviour {
+    public bool inverted;
     private float height;
     private Vector3 parentStart;
     private Vector3 start;
@@ -16,7 +17,7 @@ public class Piston : MonoBehaviour {
 	void Update () {
         Vector3 diff = (transform.parent.position - parentStart);
         transform.position = start + (diff / 2);
-        transform.localScale = new Vector3(1, 1 + (diff.y/height), 1);
+        transform.localScale = new Vector3(1, (inverted ? -1 : 1) + (diff.y/height), 1);
         //transform.parent.transform.Translate(moveSpeed * Time.deltaTime);
         //transform.Translate(-moveSpeed * Time.deltaTime / 2);
         //transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y + Time.deltaTime * moveSpeed.y/2, transform.localScale.z);
